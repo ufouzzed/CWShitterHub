97e47503a258fbe0c8ac057e8b5bb35d096613fb
Maxey <94880000+ufouzzed@users.noreply.github.com>
2023-04-02 16:51:21 -0500

    Update CWLoadString.lua

--- CWLoadString.lua
+++ CWLoadString.lua
@@ -1,3 +1,3 @@
---made by JMaxeyy
+--SHOULD WORK FOR YOUR EXECUTOR
 
-local JMaxeyy = loadstring(game:HttpGet("https://raw.githubusercontent.com/ufouzzed/CWShitterHub/main/CWScript.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d867e6bc3aa5625b03c37e3cfe1d1258.lua"))()
